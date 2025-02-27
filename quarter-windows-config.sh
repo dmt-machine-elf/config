54440e4ef2f552c0d5a9f3e486502a60c6c26279
dmt-machine-elf <199597028+dmt-machine-elf@users.noreply.github.com>
2025-02-27 17:48:24 +0700

    quarter windows config

---
+++ quarter-windows-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-ne "['<Super>KP_9']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-nw "['<Super>KP_7']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-se "['<Super>KP_3']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-sw "['<Super>KP_1']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-n "['<Super>KP_8']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-s "['<Super>KP_2']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-e "['<Super>KP_6']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-w "['<Super>KP_4']"
